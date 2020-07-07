b37e4f76a8037ac19fbf8e6ea19a880d39cfb501
Maksym Danylov <maxdanylov@gmail.com>
2020-07-07 23:16:11 +0300

    completed task-04

--- js/task-02.js
+++ js/task-02.js
@@ -3,6 +3,6 @@ const total = 100;
 const ordered = 130;
 const message =
   ordered > total
-    ? 'На складе недостаточно твоаров!'
+    ? 'На складе недостаточно товаров!'
     : 'Заказ оформлен, с вами свяжется менеджер';
 console.log(message);
---
+++ js/task-04.js
@@ -0,0 +1,24 @@
+'use strict';
+const credits = 23580;
+const pricePerDroid = 3000;
+let userAnswer = prompt('Пожалуйста, введите необходимое количество дроидов');
+if (userAnswer === null) {
+  console.log('Отменено пользователем!');
+} else {
+  let amountDroid = Number.parseInt(userAnswer);
+  console.log(amountDroid);
+  if (Number.isNaN(amountDroid)) {
+    prompt('Вы ввели некорректное значение вашей покупки.');
+  } else {
+    const totalPrice = amountDroid * pricePerDroid;
+    if (totalPrice > credits) {
+      console.log('Недостаточно средств на счету!');
+    } else {
+      console.log(
+        `Вы купили ${amountDroid} дроидов, на счету осталось ${
+          credits - totalPrice
+        } кредитов.`,
+      );
+    }
+  }
+}
